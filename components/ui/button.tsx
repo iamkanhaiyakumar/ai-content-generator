0574e76989cfb80badb62681f9145059514a3bee
Raj Rawariya <98628762+Raj100@users.noreply.github.com>
2024-10-20 20:03:25 +0530

    fix dependency errors and vulnerabilities

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,7 +1,9 @@
+'use client';
+
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
-import { motion } from "framer-motion"
+import {motion} from 'framer-motion'
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
