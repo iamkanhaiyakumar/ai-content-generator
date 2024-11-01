568ed1a160d46c9aa61596f1d57156e3879c5822
Kanhaiya Kumar <kanhaiyak0104@gmail.com.com>
2024-11-01 10:42:50 +0530

    feat: Add UpdateCreditUsageContext and UserSubscriptionContext; update dashboard layout to integrate new contexts

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,6 +4,8 @@ import React, { useState } from 'react';
 import SideNav from './_components/SideNav';
 import Header from './_components/Header';
 import { TotalUsageContext } from '../(context)/TotalUsageContext';
+import { UpdateCreditUsageContext } from '../(context)/UpdateCreditUsageContext';
+import UserSubscriptionContext from '../(context)/UserSubscriptionContext';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -12,9 +14,13 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   // Initialize state for total usage
   const [totalUsage, setTotalUsage] = useState<number>(0);
+  const [userSubscription, setUserSubscription] = useState<boolean>(false);
+  const [UpdateCreditUsageContext ,setUpdateCreditUsage] = useState<any>() 
 
   return (
     <TotalUsageContext.Provider value={{ totalUsage, setTotalUsage }}>
+      {/* <UserSubscriptionContext.Provider value={{userSubscription, setUserSubscription}}> */}
+      <UpdateCreditUsageContext.Provider value={{UpdateCreditUsageContext ,setUpdateCreditUsage}}>
       <div className="h-screen flex">
         {/* Sidebar for larger screens */}
         <aside className="md:w-64 hidden md:block fixed">
@@ -27,7 +33,10 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
           <main>{children}</main>
         </div>
       </div>
+      </UpdateCreditUsageContext.Provider>
+      {/* </UserSubscriptionContext.Provider> */}
     </TotalUsageContext.Provider>
+    
   );
 };
 
