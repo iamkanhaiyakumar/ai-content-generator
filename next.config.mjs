d0fef128c4d56f1a902afeef16f8f10f27dab4f1
Sneha <229462648+Sneha6657@users.noreply.github.com>
2026-05-21 11:46:03 +0530

    fix: replace <img> tags with Next.js Image component

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        domains:['img.icons8.com', 'cdn-icons-png.flaticon.com']
-    },
+        remotePatterns: [
+      { protocol: 'https', hostname: 'img.icons8.com' },
+      { protocol: 'https', hostname: 'cdn-icons-png.flaticon.com' },
+      { protocol: 'https', hostname: 'cdni.iconscout.com' },
+      { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
+    ],
+  },
     env: {
         DATABASE_URL: process.env.NEXT_PUBLIC_DATABASE_URL,
     },
