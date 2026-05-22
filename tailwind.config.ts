cfc41344ebfcd38fd685bd904b405c9cfbcc8409
Sneha <229462648+Sneha6657@users.noreply.github.com>
2026-05-22 15:14:48 +0530

    feat: render AI markdown responses properly

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -75,7 +75,7 @@ const config: Config = {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate"), addVariablesForColors],
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography"), addVariablesForColors],
 };
 export default config;
 
