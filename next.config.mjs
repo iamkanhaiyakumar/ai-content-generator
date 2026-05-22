aa2f550489d5d39f9307ee1bf31b555425d0969c
Sneha <229462648+Sneha6657@users.noreply.github.com>
2026-05-22 11:40:06 +0530

    Revert unintended config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images:{
-    domains:[
-      'img.icons8.com',
-      'cdn-icons-png.flaticon.com',
-      'cdni.iconscout.com',
-      'avatars.githubusercontent.com',
-    ]
-  },
-  env: {
-    DATABASE_URL: process.env.NEXT_PUBLIC_DATABASE_URL,
-  },
+    images:{
+        domains:['img.icons8.com', 'cdn-icons-png.flaticon.com']
+    },
+    env: {
+        DATABASE_URL: process.env.NEXT_PUBLIC_DATABASE_URL,
+    },
 };
 
 export default nextConfig;
